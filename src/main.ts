e8ff58dd1497c5b6298640d181f133994f4acaeb
Fran-Bar03 <papihachi_vlk@outlook.com>
2024-11-07 09:22:40 -0700

    segundo avance

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { appRouterProviders } from './app/app.routes';
-import { importProvidersFrom } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 
-bootstrapApplication(AppComponent, 
-  {providers : [appRouterProviders,importProvidersFrom(RouterModule)]}
+bootstrapApplication(AppComponent, {providers : [appRouterProviders,importProvidersFrom(RouterModule, BrowserAnimationsModule)]}
 )
   .catch((err) => console.error(err));
